e28ef92e3c6bd296070fa880bedc62d7f08695e1
Philipp <philipp@pilleslife.de>
2017-10-12 11:28:00 +0200

    AddNoiseLaplace only changes coefficients higher than threshold

--- src/privacyTransformation/implementation/AddNoiseLaplace.java
+++ src/privacyTransformation/implementation/AddNoiseLaplace.java
@@ -8,12 +8,13 @@ import java.util.stream.IntStream;
 
 public class AddNoiseLaplace implements PrivacyTransformation {
 
-    private final double MAX_SIGMA = 25;
+    private final double MAX_LAMBDA = 50;
     private double mappedLambda;
+    private double mappedSigma;
 
     @Override
     public HaarData transform(PrivacyTransformationHandler transformationHandler, HaarData haarData, double configurationValue) {
-        setMappedLambda(configurationValue);
+        mapParameters(transformationHandler, haarData, configurationValue);
         double[][] coefficients = haarData.getCoefficients();
         HaarData transformedData = haarData.clone();
 
@@ -25,17 +26,21 @@ public class AddNoiseLaplace implements PrivacyTransformation {
         return transformedData;
     }
 
-    private void setMappedLambda(double configurationValue) {
-        mappedLambda = DataUtil.mapToInterval(configurationValue, 0.1, MAX_SIGMA);
+    private void mapParameters(PrivacyTransformationHandler transformationHandler, HaarData coeffs, double configurationValue) {
+        double maxCoefficientForThreshold = transformationHandler.getMaxCoefficientForThresholds(coeffs);
+        mappedSigma = DataUtil.mapToInterval(configurationValue, maxCoefficientForThreshold, 0);
+        mappedLambda = DataUtil.mapToInterval(configurationValue, 0.1, MAX_LAMBDA);
     }
 
     private double[] adaptLevel(PrivacyTransformationHandler transformationHandler, HaarData haarData, int level) {
+        double[] coefficientsForThreshold = transformationHandler.getCoefficentsForThreshold()[level];
         double[] dataRowCopy = haarData.getCoefficients()[level];
 
         int weight = (int) Math.pow(2, haarData.getCoefficients().length - level);
         double magnitude = mappedLambda / weight;
         IntStream.range(0, dataRowCopy.length)
                 .filter(index -> !transformationHandler.isFixedValue(level, index))
+                .filter(index -> coefficientsForThreshold[index] > mappedSigma)
                 .forEach(index -> {
                     // Change sign of noise if coefficient was root of switching transformation
                     int sign = 1;
@@ -55,7 +60,7 @@ public class AddNoiseLaplace implements PrivacyTransformation {
 
     @Override
     public double getConfigurationValue(PrivacyTransformationHandler transformationHandler, HaarData haarData, double configurationValue) {
-        setMappedLambda(configurationValue);
+        mapParameters(transformationHandler, haarData, configurationValue);
         return mappedLambda;
     }
 }
